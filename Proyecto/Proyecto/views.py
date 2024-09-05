50f9e3f84b8b33ffab344fcd84fe6ca48f9a56a5
rjsoloaga <javiersoloaga5@gmail.com>
2024-09-05 01:06:22 -0300

    primer commit

---
+++ Proyecto/Proyecto/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+# Request: Para realizar peticiones.
+# HttpResponse: Para enviar la respuesta usando el protocolo HTTP.
+
+# Esto es una vista de bienvenida
+def bienvenida(request):
+    return HttpResponse("<h1 style='color: red;'>Bienvenido a Body Center</h1>")
+
+def horaActual(request):
+    respuesta = "<h2>La Hora es: {0}</h2>".format(datetime.datetime.now().strftime("%A %d/%m/%Y %H:%M:%S"))
+    return HttpResponse(respuesta)
+
+def index(request):
+    # Abrimos el documento que contiene la plantilla:
+    plantillaExterna = open("C:\\Users\\javie\\OneDrive\\Escritorio\\SisTurDj\\Proyecto\\Proyecto\\templates\\index.html")
+    # Cargamos el documento en una variable de tipo 'Template':
+    template = Template(plantillaExterna.read())
+    # Cerrar el documento externo que hemos abierto:
+    plantillaExterna.close()
+    # Crear un contexto:
+    contexto = Context()
+    # Renderizar el documento:
+    documento = template.render(contexto)
+    
+    return HttpResponse(documento)
