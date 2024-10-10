5e090615d4f2b25961a974fba716ca64f846e938
rjsoloaga <javiersoloaga5@gmail.com>
2024-10-10 00:17:36 -0300

    modelo usuario y migraciones

--- Proyecto/Aplicaciones/Usuarios/models.py
+++ Proyecto/Aplicaciones/Usuarios/models.py
@@ -5,4 +5,14 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     imagen = models.ImageField(upload_to='usuarios', default='usuarios/user_avatar_profile_icon.png')
     is_colaborador = models.BooleanField(default=False)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',  # Nombre personalizado
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',  # Nombre personalizado
+        blank=True,
+    )
     
